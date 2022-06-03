bd11b76d451825ae2d38fe2efd946739e2e7b20f
William Østensen <william-98-@hotmail.com>
2022-06-03 15:36:45 +0200

    added issues page and documentation

---
+++ issues.js
@@ -0,0 +1,13 @@
+// function splitScroll() {
+//   const controller = new ScrollMagic.Controller();
+
+//   new ScrollMagic.Scene({
+//     triggerElement: ".issues-container",
+//     triggerHook: 0,
+//   })
+//     .setPin(".issues-text-container")
+//     .addIndicators()
+//     .addTo(controller);
+// }
+
+// splitScroll();
